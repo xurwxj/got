eaf1bc80e5a01528df42c558a37025df95582b08
victor <victor@xurw.com>
2020-08-19 17:56:37 +0800

    add content-disposition for GetFilename

--- filename.go
+++ filename.go
@@ -1,8 +1,10 @@
 package got
 
 import (
+	"net/http"
 	"net/url"
 	"path/filepath"
+	"strings"
 )
 
 // DefaultFileName is the fallback name for GetFilename.
@@ -14,6 +16,19 @@ func GetFilename(URL string) string {
 	if u, err := url.Parse(URL); err == nil && filepath.Ext(u.Path) != "" {
 
 		return filepath.Base(u.Path)
+	}
+
+	res, err := http.Head(URL)
+	if err == nil {
+		header := res.Header
+		if hcd, ok := header["Content-Disposition"]; ok && len(hcd) > 0 {
+			hcds := strings.Split(hcd[0], "=")
+			if len(hcds) > 1 {
+				if filename := hcds[1]; filename != "" {
+					return filepath.Base(filename)
+				}
+			}
+		}
 	}
 
 	return DefaultFileName
